ab6364e71fc01b4dc4cf1f06b04ac1dd854467e1
Andor <andorhorvat@gmail.com>
2022-06-08 20:29:43 +0200

    First commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestQueueEmails;
 use App\Http\Controllers\JobController;
-
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +16,25 @@ use App\Http\Controllers\JobController;
 |
 */
 
-Route::get('/', function () {
+/**
+ * This app is here.
+ * http://127.0.0.1:8000/dvd-rental
+ */
+Route::get('/dvd-rental', function () {
     return view('welcome');
 });
+
+/**
+ * This is for testing if the db is connected to the app.
+ */
+Route::get('/test-db', function () {
+    if(DB::connection()->getDatabaseName()){
+        echo "Connected sucessfully to database ".DB::connection()->getDatabaseName().".";
+    }
+
+    return view('welcome');
+});
+
 
 
 Route::get('send-mail','App\Http\Controllers\EmailController@sendMail');
